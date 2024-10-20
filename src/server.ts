916b9296127b4a4d8494c7fd30f4a5f912f50d5f
GabrielEurico <gabriel.eurico99@outlook.com>
2024-10-20 13:56:32 -0300

    Created first routes

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,29 @@
-console.log("Hello")
+import chalk from "chalk";
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT ?? 3030;
+
+app.use(express.json())
+
+app.get("/", (_: Request, res: Response) => {
+  res.status(200).json({
+    message: " Boas vindas à DioBank API",
+  });
+});
+
+app.post("/user", (req: Request, res: Response) => {
+  const body = req.body;
+  console.log(body);
+
+  res.status(201).json({
+    message: "Usuário criado com sucesso!",
+  });
+});
+
+app.listen(PORT, () => {
+  const log = console.log;
+
+  log(chalk.green(`  ✔ Server initial on port: ${PORT}\n`));
+  log(` 🔗 link: ${chalk.blue(`http://localhost:${PORT}`)}`);
+});
